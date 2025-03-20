b9684a195a1d79064df938689a89714452e2dbe5
IncuksukEi <incuksuk123@gmail.com>
2025-03-20 21:58:27 +0700

    add entity

---
+++ hustfood/hustfood/backend/src/main/java/com/hustfood/web/HustfoodApplication.java
@@ -0,0 +1,13 @@
+package com.hustfood.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustfoodApplication.class, args);
+	}
+
+}
