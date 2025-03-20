e0e6b991de1677a05d75a09822001f63a64c40c4
IncuksukEi <incuksuk123@gmail.com>
2025-03-20 21:48:08 +0700

    first commmit

---
+++ hustfood/hustfood/backend/src/test/java/com/hustfood/web/HustfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hustfood.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
